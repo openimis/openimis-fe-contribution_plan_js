249655dc63d8be66e5f50a9b4a31b437ac1b5c83
olewandowski <olewandowski@soldevelo.com>
2023-04-06 10:04:43 +0200

    OP-1358: Enabling the editing of codes for PP, CP and CPB

--- src/components/PaymentPlanForm.js
+++ src/components/PaymentPlanForm.js
@@ -83,6 +83,7 @@ class PaymentPlanForm extends Component {
     !this.isMandatoryFieldsEmpty() &&
     this.isPeriodicityValid() &&
     !!this.state.jsonExtValid &&
+    !!this.props.isCodeValid &&
     !!this.state.requiredValid &&
     this.doesPaymentPlanChange();
 
