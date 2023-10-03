4a27ef33496aa2e325034a1fe20c1f8675cdcc3b
Jan <jdolkowski@soldevelo.com>
2023-10-03 16:44:19 +0200

    merge release to develop (#64)
    * CM-320: Fix displaying benefit program field in the PaymentPlan Searcher Page (#62)
    
    * CM-320: fixed payment plan benefit product in searcher
    
    * CM-320: fixed missing import
    
    * CM-320: added missing const
    
    * CM-320: added missing utils
    
    * CM-320: added processing benefitPlan object
    
    * CM-320: print state of benefit plan
    
    * CM-320: fixed objectBenefitPlan creation
    
    * CM-320: removed console logs
    
    * CM-320: fixed 'value' property
    
    * CM-322: fixed CPBDetails (#63)
    
    * CM-323: checked why contribution plan bundle details creation failed
    
    * CM-323: fixed decoding id of contribution plan bundle id
    
    ---------
    
    Co-authored-by: sniedzielski <52816247+sniedzielski@users.noreply.github.com>

--- src/actions.js
+++ src/actions.js
@@ -295,7 +295,7 @@ function formatContributionPlanBundleDetailsGQL(
         }
         ${
           !!contributionPlanBundleDetails.contributionPlanBundleId
-            ? `contributionPlanBundleId: "${contributionPlanBundleDetails.contributionPlanBundleId}"`
+            ? `contributionPlanBundleId: "${decodeId(contributionPlanBundleDetails.contributionPlanBundleId)}"`
             : ""
         }
         ${
