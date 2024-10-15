ac600cb78cd188d951df630c213fd4184a9a584c
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2024-10-15 10:56:11 +0200

    Hotfix remove duplicated decodId

--- src/components/ContributionPlanBundleContributionPlans.js
+++ src/components/ContributionPlanBundleContributionPlans.js
@@ -11,9 +11,6 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { DEFAULT_PAGE_SIZE, ROWS_PER_PAGE_OPTIONS } from "../constants";
 import CreateContributionPlanBundleDetailsDialog from "../dialogs/CreateContributionPlanBundleDetailsDialog";
 import UpdateContributionPlanBundleDetailsDialog from "../dialogs/UpdateContributionPlanBundleDetailsDialog";
-import {
-    decodeId,
-} from "@openimis/fe-core";
 
 const styles = theme => ({
     tableTitle: theme.table.title,
