49773625c1ac69a14915b3c09da9f45f47482e43
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-19 00:54:23 +0530

    fix(build): correct file extension

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.jsx'), 
       name: 'OpenIMISFeContributionPlan',
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`
+      fileName: (format) => `index.${format === 'es' ? 'es' : 'cjs'}.js`,
     },
     sourcemap: true,
     outDir: 'dist',
